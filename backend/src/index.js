385ae6cec1f25a7f0031b6c339a492798c5ace67
Zinah <zihna-19@hotmail.com>
2025-11-14 22:54:14 +0200

    trying to figure out the problem with the deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 // 404 handler
 app.use((req, res, next) => {
-  res.status(404).json({ error: "Not Found" });
+  res.status(404).json({ error: "We Can't find you route" });
 });
 
 // Centralized error handler
