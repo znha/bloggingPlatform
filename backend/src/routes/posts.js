37a5cf116d70aaae6c20f05601d9ace75b8ca404
Zinah <zihna-19@hotmail.com>
2025-11-15 16:34:02 +0200

    add all the required routes for the posts

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,8 +1,12 @@
 import express from "express";
+import * as Post from "../controllers/posts.js" 
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ status: "ok", uptime: process.uptime() });
-});
+router.get("/", Post.viewAll);
+router.get("/view/{id}", Post.viewById);
+router.get("/edit/{id}", Post.edit);
+router.get("/remove/{id}", Post.remove);
+router.get("/create", Post.create);
 
 export default router;
