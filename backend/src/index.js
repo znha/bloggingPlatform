19f5b1a0a961a370438efc2ea11d92dd1cb2fd3b
Zinah <zihna-19@hotmail.com>
2025-11-19 14:53:24 +0200

    fixing the cors problems

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 // Third-party middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // or '*' for dev
+  origin: 'https://blogging-platform-xx17.vercel.app', // or '*' for dev
   credentials: true, // if using cookies
 }));
 app.use(morgan("dev"));
